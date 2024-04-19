bcb0ff10f5ea23218a23fbf7897ec00fafa8c1bc
johnnyrpy <johnny@reepay.com>
2024-04-19 14:17:35 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "Reepay",
+            name: "ReepayCheckoutSheet",
             targets: ["ReepayCheckoutSheet"]),
     ],
     dependencies: [
