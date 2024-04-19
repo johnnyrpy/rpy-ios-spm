d83ce865fba7710b2e6331f962b2b948b21b8323
johnnyrpy <johnny@reepay.com>
2024-04-19 15:38:12 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,22 +18,23 @@ let package = Package(
         .package(url: "https://github.com/reepay/reepay-ios.git", from: "1.0.0"),
     ],
     targets: [
-        .target(
-            name: "Test",
-            path: "Sources/Test",
-            sources: ["Dummy.swift"]
-        ),
+        // .target(
+        //     name: "Test",
+        //     path: "Sources/Test",
+        //     sources: ["Dummy.swift"]
+        // ),
         // .binaryTarget(
         //     name: "ReepayCheckoutSheet",
         //     url: "https://github.com/reepay/reepay-ios/releases/download/1.0.0/Reepay.xcframework.zip",
         //     checksum: "109e9661c16691649c005ed1190f54a19c230f93beeff5c0c5569e332356dce1"
         // ),
         .target(
-            name: "ReepayCheckoutSheet",
+            name: "Test",
             dependencies: [
                 .product(name: "ReepayCheckoutSheet", package: "reepay-ios")
             ],
             path: "Sources/Test",
+            sources: ["Dummy.swift"]
         ),
     ]
 )
