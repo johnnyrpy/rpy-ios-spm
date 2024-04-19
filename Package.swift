b61560f4aa0760f938106b7c811771380ad7a7ff
johnnyrpy <johnny@reepay.com>
2024-04-19 13:55:15 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "ReepayMirror",
+            name: "ReepayCheckoutSheet",
             dependencies: [
                 .product(name: "ReepayCheckoutSheet", package: "reepay-ios")
             ]),
