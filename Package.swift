1846a6cbb9f532c7d35ba294f2508e86ff583867
johnnyrpy <johnny@reepay.com>
2024-04-19 14:50:25 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .target(
             name: "ReepayCheckoutSheet",
             dependencies: [
-                "ReepayCheckoutSheet.xcframework"
+                "/ReepayCheckoutSheet/ReepayCheckoutSheet.xcframework"
             ]),
     ]
 )
