7f89a143137f86704c434e82f370d40e859c7ac8
johnnyrpy <johnny@reepay.com>
2024-04-19 11:20:36 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Reepay SPM",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v15),
+    ],
+    products: [
+        .library(
+            name: "ReepayCheckoutSheet",
+            targets: ["ReepayCheckoutSheet"]),
+    ],
+    dependencies: [
+        .package(name: "Reepay", url: "https://github.com/reepay/reepay-ios.git", from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "ReepayCheckoutSheet",
+            path: "ReepayCheckoutSheet/ReepayCheckoutSheet.xcframework"),
+    ])
