b61f8c35e089be33b2db54943e511785b0114a88
johnnyrpy <johnny@reepay.com>
2024-04-19 12:56:13 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,6 +9,11 @@ let package = Package(
     platforms: [
         .iOS(.v15),
     ],
+    products: [
+        .library(
+            name: "ReepayCheckoutSheet",
+            targets: ["ReepayCheckoutSheet"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/reepay/reepay-ios.git", from: "1.0.0"),
     ])
